76da3fcac0d9023c1dde79de08ab2fc304edf972
Sebastian <sebastian.v.a@riseup.net>
2021-11-13 17:19:23 +0100

    revert user

--- dada-server.sh
+++ dada-server.sh
@@ -3,7 +3,7 @@
 chmod 700 duck.sh
 crontab -e
 line="*/5 * * * * ~/dada-lab-dnsscript/duck.sh >/dev/null 2>&1"
-(crontab -u root -l; echo "$line" ) | crontab -u root -
+(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 ./duck.sh
 cat duck.log
 
