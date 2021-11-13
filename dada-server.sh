33d24502ef5ad03394e7093d748b85bb038ab00b
Sebastian <sebastian.v.a@riseup.net>
2021-11-13 17:14:38 +0100

    changed repo path

--- dada-server.sh
+++ dada-server.sh
@@ -2,7 +2,7 @@
 
 chmod 700 duck.sh
 crontab -e
-line="*/5 * * * * ~/duckdns/duck.sh >/dev/null 2>&1"
+line="*/5 * * * * ~/dada-lab-dnsscript/duck.sh >/dev/null 2>&1"
 (crontab -u root -l; echo "$line" ) | crontab -u root -
 ./duck.sh
 cat duck.log
