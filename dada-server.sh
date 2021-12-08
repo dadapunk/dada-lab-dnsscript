42879d2d74e1c261c9600f0d40d3e0afad78e650
Sebastian <sebastian.v.a@riseup.net>
2021-12-08 13:55:51 +0100

    intall lines

--- dada-server.sh
+++ dada-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update -y && sudo apt upgrade -y
+sudo rm -rf /opt/plexguide && sudo rm -rf /opt/pgstage && sudo apt install curl -y
 
 chmod 700 duck.sh
 crontab -e
@@ -6,5 +8,11 @@ line="*/5 * * * * ~/dada-lab-dnsscript/duck.sh >/dev/null 2>&1"
 (crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 ./duck.sh
 cat duck.log
+
+echo should i continue with the installation?
+read
+
+curl -s https://raw.githubusercontent.com/PGBlitz/Install/v8.5/install.sh | sudo -H sh
+
 
 
