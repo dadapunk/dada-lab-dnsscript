1708f6a68721906f5dc4fa820638ac6c21a761d6
Sebastian <sebastian.v.a@riseup.net>
2022-01-02 18:18:13 +0100

    change to crontab

--- dada-server.sh
+++ dada-server.sh
@@ -3,9 +3,12 @@ sudo apt update -y && sudo apt upgrade -y
 sudo rm -rf /opt/plexguide && sudo rm -rf /opt/pgstage && sudo apt install curl -y
 
 chmod 700 duck.sh
-crontab -e
-line="*/5 * * * * ~/dada-lab-dnsscript/duck.sh >/dev/null 2>&1"
-(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
+#todo
+#insert line at crontab without enter the editor
+#crontab -e
+#line="*/5 * * * * ~/dada-lab-dnsscript/duck.sh >/dev/null 2>&1"
+#(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
+crontab -l | { cat; echo "*/5 * * * * ~/dada-lab-dnsscript/duck.sh >/dev/null 2>&1"; } | crontab -
 ./duck.sh
 cat duck.log
 
